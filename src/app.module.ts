5a846c7907a50b6b3337b9a7afa9758eb9ccb9d8
Tu Nombre <diley963@gmail.com>
2024-09-07 11:04:50 -0500

    resolver conflictos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,22 @@
-import { ConfigModule, ConfigService } from '@nestjs/config';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Usuario } from './usuarios/usuario.entity';
 import { UsuariosModule } from './usuarios/usuarios.module';
-import { Usuario } from './usuarios/usuario.entity'; 
+import { Usuario } from './usuarios/usuario.entity';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config'; 
+import { ConfigModule, ConfigService } from '@nestjs/config'; // Importa ConfigService
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
+      useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('DATABASE_HOST'),
         port: configService.get<number>('DATABASE_PORT'),
@@ -23,28 +24,11 @@ import { ConfigModule } from '@nestjs/config';
         password: configService.get<string>('DATABASE_PASSWORD'),
         database: configService.get<string>('DATABASE_NAME'),
         entities: [Usuario],
-        synchronize: true, // No recomendado en producción
+        synchronize: true, 
         logging: true,
       }),
-    ConfigModule.forRoot({
-      isGlobal: true, // Asegúrate de que ConfigModule sea global
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres', // Usuario de PostgreSQL
-      password: 'Seguridad2024', // Contraseña de PostgreSQL
-      database: 'postgres', // Nombre de la base de datos
-      entities: [Usuario], // Asegúrate de incluir todas las entidades necesarias
-      synchronize: true, // No recomendado en producción
-      logging: true, // Opcional para depuración
     }),
     UsuariosModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
-    }),
     AuthModule,
   ],
   controllers: [AppController],
